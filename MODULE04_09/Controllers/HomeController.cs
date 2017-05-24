9de765021f02554a871bd942d57e05dee4dfb775
van chung chen <auxo_86@hotmail.com>
2017-05-25 00:40:59 +0800

    使用了Models底下的ProductSystem，所以要using MODULE04_09.Models

--- MODULE04_09/Controllers/HomeController.cs
+++ MODULE04_09/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MODULE04_09.DAL;
+//使用了Models底下的ProductSystem，所以要using MODULE04_09.Models
 using MODULE04_09.Models;
 
 namespace MODULE04_09.Controllers
