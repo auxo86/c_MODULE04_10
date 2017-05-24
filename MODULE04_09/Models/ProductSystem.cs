93fe7b93b61cc0fa1b952792f7fd3047b178a88a
van chung chen <auxo_86@hotmail.com>
2017-05-24 22:46:06 +0800

    加入專案檔案。

---
+++ MODULE04_09/Models/ProductSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODULE04_09.DAL;
+
+namespace MODULE04_09.Models
+{
+    public class ProductSystem
+    {
+        public Product GetProductByID(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var query = from p in db.Products
+                        where p.ProductID == id
+                        select p;
+
+            //只列第一筆
+            var result = query.First();
+
+            return result;
+
+        }
+
+        public IEnumerable<Product> GetProductsByCategoryID(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var query = from p in db.Products
+                        where p.CategoryID == id
+                        select p;
+
+            var result = query.ToList();
+            return result;
+        }
+    }
+}
